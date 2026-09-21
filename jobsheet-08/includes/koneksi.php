20df846da655cbed2e606682edf7374e92908a73
fawaswali <fawaswali@gmail.com>
2026-09-21 13:39:47 +0700

    revisi

--- jobsheet-08/includes/koneksi.php
+++ jobsheet-08/includes/koneksi.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST') ?: 'aws-0-ap-southeast-1.pooler.supabase.com';
 $port = getenv('DB_PORT') ?: '6543';
 $dbname = getenv('DB_NAME') ?: 'postgres';
 $user = getenv('DB_USER') ?: 'postgres.mpycrxqzjfmqqafoxpew';
-$password = getenv('DB_PASS') ?: 'password_asli_supabase_kamu';
+$password = getenv('DB_PASS') ?: 'sg95WPSX2YuRgV91';
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
