30c9a53d1ae29ebd409941007b48becc1f6aae62
fawaswali <fawaswali@gmail.com>
2026-09-21 13:38:27 +0700

    fix variable

--- jobsheet-08/includes/koneksi.php
+++ jobsheet-08/includes/koneksi.php
@@ -3,17 +3,16 @@ $host = getenv('DB_HOST') ?: 'aws-0-ap-southeast-1.pooler.supabase.com';
 $port = getenv('DB_PORT') ?: '6543';
 $dbname = getenv('DB_NAME') ?: 'postgres';
 $user = getenv('DB_USER') ?: 'postgres.mpycrxqzjfmqqafoxpew';
-$password = getenv('DB_PASS') ?: 'password_supabase_kamu';
+$password = getenv('DB_PASS') ?: 'password_asli_supabase_kamu';
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-    
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
     
     if ($host !== 'localhost') {
         $dsn .= ";sslmode=require";
     }
 
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
