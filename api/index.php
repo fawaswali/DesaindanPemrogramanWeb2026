9b3289d163e812aae7b228043714bc6945a358e6
fawaswali <fawaswali@gmail.com>
2026-09-26 15:07:28 +0700

    api/index.php

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$targetFile = dirname(__DIR__) . $requestUri;
+
+if (is_dir($targetFile)) {
+    $targetFile = rtrim($targetFile, '/') . '/index.php';
+}
+
+if (file_exists($targetFile) && pathinfo($targetFile, PATHINFO_EXTENSION) === 'php') {
+    chdir(dirname($targetFile));
+    require $targetFile;
+    exit;
+}
+
+http_response_code(404);
+echo "Halaman tidak ditemukan.";
